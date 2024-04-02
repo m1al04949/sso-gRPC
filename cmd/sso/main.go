de71a4b2c6f073da28c5b7a7adc4b87a7f5b0f5c
Ivan Gorbunov <m1al04949@gmail.com>
2024-04-02 23:05:12 +0300

    Init config and log

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,44 @@
 package main
 
-func main() {
-	// TODO: Инициализация объекта конфига
+import (
+	"log/slog"
+	"os"
 
-	// TODO: Инициализация логгера
+	"github.com/m1al04949/sso-gRPC/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// Config initialize
+	cfg := config.MustLoad()
+
+	// Logger initialize
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.Any("cfg", cfg))
 
 	// TODO: Инициализация приложения
 
 	// TODO: Запуск gRPC-сервис
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
