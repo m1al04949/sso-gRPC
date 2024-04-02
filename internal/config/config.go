bdced0b071601532c0dd8da6552ffb73308be048
Ivan Gorbunov <m1al04949@gmail.com>
2024-04-02 23:04:53 +0300

    Init config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
@@ -14,6 +20,38 @@ type GRPCConfig struct {
 	Timeout time.Duration `yaml:"timeout"`
 }
 
-// func MustLoad() *Config {
+func MustLoad() *Config {
+	// Path to Config
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
 
-// }
+	// Check path is exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	var cfg Config
+
+	// Parse config file
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	// --config="path/to/config.yaml"
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
