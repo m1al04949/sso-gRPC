437cf5ab56a914071e4dd30aa62deb232ecf0c9b
Ivan Gorbunov <m1al04949@gmail.com>
2025-07-08 15:17:33 +0300

    fix: problems. add: app, close

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,151 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/m1al04949/sso-gRPC/internal/domain/models"
+	"github.com/m1al04949/sso-gRPC/internal/storage"
+	"modernc.org/sqlite"
+	sqlerr "modernc.org/sqlite/lib"
+)
+
+type Storage struct {
+	db  *sql.DB
+	log *slog.Logger
+}
+
+// New instance of storage
+func New(log *slog.Logger, storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite", fmt.Sprintf("file:%s", storagePath))
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{
+		db:  db,
+		log: log}, nil
+}
+
+// SaveUser saving new user
+func (s *Storage) SaveUser(ctx context.Context, email string, passHash []byte) (int64, error) {
+	const op = "storage.sqlite.SaveUser"
+
+	stmt, err := s.db.Prepare("INSERT INTO users(email, pass_hash) VALUES(?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(ctx, email, passHash)
+	if err != nil {
+		var sqliteErr *sqlite.Error
+
+		if errors.As(err, &sqliteErr) {
+			switch sqliteErr.Code() {
+			case sqlerr.SQLITE_CONSTRAINT_UNIQUE:
+				return 0, fmt.Errorf("%s: %w", op, storage.ErrUserExists)
+			default:
+				return 0, fmt.Errorf("%s: sqlite error [%d]: %w", op, sqliteErr.Code(), err)
+			}
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+// User returns user by email
+func (s *Storage) User(ctx context.Context, email string) (models.User, error) {
+	const op = "storage.sqlite.User"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash FROM users WHERE email = ?")
+	if err != nil {
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRowContext(ctx, email)
+
+	var user models.User
+	err = row.Scan(&user.ID, &user.Email, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
+}
+
+// IsAdmin check user is admin
+func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	const op = "storage.sqlite.IsAdmin"
+
+	stmt, err := s.db.Prepare("SELECT is_admin FROM users WHERE  id = ?")
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRowContext(ctx, userID)
+
+	var isAdmin bool
+	err = row.Scan(&isAdmin)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
+}
+
+// App returns some info about current app
+func (s *Storage) App(ctx context.Context, appID int) (models.App, error) {
+	const op = "storage.sqlite.App"
+
+	stmt, err := s.db.Prepare("SELECT id, name, secret FROM apps WHERE id = ? ")
+	if err != nil {
+		return models.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	row := stmt.QueryRowContext(ctx, appID)
+
+	var app models.App
+	err = row.Scan(&app.ID, &app.Name, &app.Secret)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.App{}, fmt.Errorf("%s: %w", op, storage.ErrAppNotFound)
+		}
+
+		return models.App{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return app, nil
+}
+
+// Close closing storage
+func (s *Storage) Close() {
+	if err := s.db.Close(); err != nil {
+		s.log.Error(err.Error())
+		return
+	}
+	s.log.Info("storage stopped successfully")
+}
