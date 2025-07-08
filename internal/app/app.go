f2c40db4ebe79b0f1cd0a00306090d510852272c
Ivan Gorbunov <m1al04949@gmail.com>
2025-07-08 15:21:03 +0300

    init storage

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,19 +5,30 @@ import (
 	"time"
 
 	"github.com/m1al04949/sso-gRPC/internal/app/grpcapp"
+	"github.com/m1al04949/sso-gRPC/internal/services/auth"
+	"github.com/m1al04949/sso-gRPC/internal/storage/sqlite"
 )
 
 type App struct {
 	GRPCSrv *grpcapp.App
+	Storage *sqlite.Storage
 }
 
 func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
-	// TODO: init storage
+	// Init storage
+	storage, err := sqlite.New(log, storagePath)
+	if err != nil {
+		panic(err)
+	}
 
-	// TODO: init auth service
-	grpcApp := grpcapp.New(log, grpcPort)
+	// Init auth service
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	// Init app
+	grpcApp := grpcapp.New(log, authService, grpcPort)
 
 	return &App{
 		GRPCSrv: grpcApp,
+		Storage: storage,
 	}
 }
